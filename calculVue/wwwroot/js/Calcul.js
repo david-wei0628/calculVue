181feed97d64a73b06f77499f5aea2c27653aff3
david-wei0628 <62085333+david-wei0628@users.noreply.github.com>
2022-04-15 14:16:30 +0800

    加入專案檔案。

---
+++ calculVue/wwwroot/js/Calcul.js
@@ -0,0 +1,33 @@
+﻿const vm = Vue.createApp({
+    data() {
+        return {
+            sum: 0,
+            tempvar:0
+        }
+    },
+    methods: {
+        plus: function () {
+            this.tempvar = this.tempvar + this.sum;
+            this.sum = 0;
+        },
+        sub: function () {
+            this.tempvar = this.tempvar - this.sum;
+            this.sum = 0;
+        },
+        mul: function () {
+            this.tempvar = this.tempvar * this.sum;
+            this.sum = 0;
+        },
+        div: function () {
+            this.tempvar = this.tempvar / this.sum;
+            this.sum = 0;
+        },
+        equ: function () {
+            this.sum = this.tempvar;
+            this.tempvar = 0;
+        }
+
+    }
+});
+
+vm .mount('#app');
